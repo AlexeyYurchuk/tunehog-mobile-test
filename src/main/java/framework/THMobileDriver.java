09fb322f5e532ad243fd6db160d8bb0c405ca803
art-tykh <art.tykh@gmail.com>
2014-05-28 15:06:19 +0300

    Add appActivity capability

--- src/main/java/framework/THMobileDriver.java
+++ src/main/java/framework/THMobileDriver.java
@@ -106,7 +106,7 @@ public class THMobileDriver implements Loggable {
         capabilities.setCapability("app", fullAppName);
         if (platformName.equals(ANDROID)) {
             capabilities.setCapability("appPackage", "com.rnrmusic.radio");
-            capabilities.setCapability("appActivity", ".activities.MainActivity");
+            capabilities.setCapability("appActivity", ".RadioApplication");
         }
         try {
             driver = new AppiumDriver(new URL(urlString), capabilities);
