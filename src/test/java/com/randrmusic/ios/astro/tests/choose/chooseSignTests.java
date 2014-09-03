0f53cb0072dd91c6bb5c6c7b6e58177e41cfc099
Artem <art.tykh@gmail.com>
2014-09-03 13:37:33 +0300

    Choose Sign test.

---
+++ src/test/java/com/randrmusic/ios/astro/tests/choose/chooseSignTests.java
@@ -0,0 +1,10 @@
+package com.randrmusic.ios.astro.tests.choose;
+
+import com.randrmusic.ios.astro.tests.AstroBaseTest;
+
+/**
+ * Created by admin on 07.07.14.
+ */
+public class chooseSignTests extends AstroBaseTest {
+
+}
